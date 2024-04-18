9073da45b4cdb935433b24a8cc9de4d06b4763f8
Akhil Eldho <akhil.p.eldho@gmail.com>
2024-04-18 17:07:14 +1200

    MSA

---
+++ ContosoPizza/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpGet("listedtry")]
+    public IActionResult GetList()
+    {
+        return Ok("Works check 2");
+    }
+
+    [HttpGet("test")]
+    public IActionResult GetTest()
+    {
+        return Ok("Works test");
+    }
+
+}
+
---
+++ SimpleWebsite/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+//making a function so when someone clicks button goes to dark mode
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+
+    //changing the button name
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+});
+
+
+
+
